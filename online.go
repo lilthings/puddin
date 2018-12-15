2d9eb04120c92c3be5db694d0c701cebc28ab625
lilthings <41975145+lilthings@users.noreply.github.com>
2018-12-15 02:17:36 -0800

    delay alerts after a pwd show 5 minutes

--- online.go
+++ online.go
@@ -12,6 +12,7 @@ import (
 )
 
 func watchOnlineRooms(affId string, client *elastic.Client, ctx context.Context) {
+	var lastInPasswordShow time.Time
 	for {
 		bulk := client.Bulk()
 
@@ -63,38 +64,41 @@ func watchOnlineRooms(affId string, client *elastic.Client, ctx context.Context)
 						if !puddinPublic {
 							v, err := getViewerCount(alertRoom)
 							if err != nil || v == 0 {
+								lastInPasswordShow = time.Now()
 								fmt.Println("err getting viewers, maybe pwd: ", v, err)
 							} else {
 								fmt.Println("viewers: ", v)
-								discord.ChannelMessageSendEmbed(notificationChannelId, &discordgo.MessageEmbed{
-									URL:   "https://chaturbate.com/" + alertRoom,
-									Title: alertRoom + " is now online!",
-									Color: 0xff008c,
-									// Footer: &discordgo.MessageEmbedFooter{Text: "Made using the discordgo library"},
-									Image: &discordgo.MessageEmbedImage{
-										URL: fmt.Sprintf("https://roomimg.stream.highwebmedia.com/ri/%s.jpg?%d", alertRoom, time.Now().Unix()),
-									},
-									Fields: []*discordgo.MessageEmbedField{
-										{
-											Name:   "Status",
-											Value:  "\u200b" + value.CurrentShow,
-											Inline: true,
+								if time.Since(lastInPasswordShow) > 5*time.Minute {
+									discord.ChannelMessageSendEmbed(notificationChannelId, &discordgo.MessageEmbed{
+										URL:   "https://chaturbate.com/" + alertRoom,
+										Title: alertRoom + " is now online!",
+										Color: 0xff008c,
+										// Footer: &discordgo.MessageEmbedFooter{Text: "Made using the discordgo library"},
+										Image: &discordgo.MessageEmbedImage{
+											URL: fmt.Sprintf("https://roomimg.stream.highwebmedia.com/ri/%s.jpg?%d", alertRoom, time.Now().Unix()),
 										},
-										{
-											Name:   "Viewers",
-											Value:  "\u200b" + fmt.Sprintf("%d", value.NumUsers),
-											Inline: true,
+										Fields: []*discordgo.MessageEmbedField{
+											{
+												Name:   "Status",
+												Value:  "\u200b" + value.CurrentShow,
+												Inline: true,
+											},
+											{
+												Name:   "Viewers",
+												Value:  "\u200b" + fmt.Sprintf("%d", value.NumUsers),
+												Inline: true,
+											},
+											{
+												Name:   "Title",
+												Value:  "\u200b" + stripTitleTags(value.RoomSubject),
+												Inline: false,
+											},
 										},
-										{
-											Name:   "Title",
-											Value:  "\u200b" + stripTitleTags(value.RoomSubject),
-											Inline: false,
-										},
-									},
-								})
-								discord.UpdateStatus(0, "Watchin Puddin :)")
+									})
+									discord.UpdateStatus(0, "Watchin Puddin :)")
 
-								puddinPublic = true
+									puddinPublic = true
+								}
 							}
 						}
 						foundPuddin = true
