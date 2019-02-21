beedd43fdd5a59e6cf00aa4c07430096b248a331
lilthings <41975145+lilthings@users.noreply.github.com>
2019-02-21 15:41:30 -0800

    sessions omitempty

--- session.go
+++ session.go
@@ -3,30 +3,30 @@ package main
 import "time"
 
 type Session struct {
-	Username string `json:"username"`
-	ShowType string `json:"show_type"`
-	Gender   string `json:"gender"`
-	Location string `json:"location"`
-	Birthday string `json:"birthday"`
+	Username string `json:"username,omitempty"`
+	ShowType string `json:"show_type,omitempty"`
+	Gender   string `json:"gender,omitempty"`
+	Location string `json:"location,omitempty"`
+	Birthday string `json:"birthday,omitempty"`
 
-	AverageViewers int64 `json:"average_viewers"`
-	MaxViewers     int64 `json:"max_viewers"`
+	AverageViewers int64 `json:"average_viewers,omitempty"`
+	MaxViewers     int64 `json:"max_viewers,omitempty"`
 
-	StartFollowers int64 `json:"start_followers"`
-	EndFollowers   int64 `json:"end_followers"`
-	MinFollowers   int64 `json:"min_followers"`
-	MaxFollowers   int64 `json:"max_followers"`
-	DeltaFollowers int64 `json:"delta_followers"`
+	StartFollowers int64 `json:"start_followers,omitempty"`
+	EndFollowers   int64 `json:"end_followers,omitempty"`
+	MinFollowers   int64 `json:"min_followers,omitempty"`
+	MaxFollowers   int64 `json:"max_followers,omitempty"`
+	DeltaFollowers int64 `json:"delta_followers,omitempty"`
 
-	StartTime   time.Time     `json:"start_time"`
-	EndTime     time.Time     `json:"end_time"`
-	DurationNs  time.Duration `json:"duration_ns"`
-	DurationStr string        `json:"duration_str"`
+	StartTime   time.Time     `json:"start_time,omitempty"`
+	EndTime     time.Time     `json:"end_time,omitempty"`
+	DurationNs  time.Duration `json:"duration_ns,omitempty"`
+	DurationStr string        `json:"duration_str,omitempty"`
 
-	StartRank int64 `json:"start_rank"`
-	EndRank   int64 `json:"end_rank"`
-	MinRank   int64 `json:"min_rank"`
-	MaxRank   int64 `json:"max_rank"`
+	StartRank int64 `json:"start_rank,omitempty"`
+	EndRank   int64 `json:"end_rank,omitempty"`
+	MinRank   int64 `json:"min_rank,omitempty"`
+	MaxRank   int64 `json:"max_rank,omitempty"`
 
 	viewersAvgTotal int64
 	viewersAvgCount int64
